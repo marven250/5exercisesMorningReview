22c79d88b7e6fe3c1c8b0da3317f90307c31d5f1
Angelo Longoria <42070067+arlongoria93@users.noreply.github.com>
2022-12-22 13:51:59 -0800

    magnifying solution draft

--- DOM Events/index.js
+++ DOM Events/index.js
@@ -1,8 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // cookie clicker
   const cookie = document.querySelector("#cookie");
   const counter = document.querySelector("#counter");
-  console.log(cookie);
   cookie.addEventListener("click", function () {
     counter.textContent++;
+  });
+
+  // magnify text
+  const text = document.querySelector("#text");
+  const increaseFont = document.querySelector("#plusFont");
+  const decreaseFont = document.querySelector("#minusFont");
+  let currentFontSize = parseInt(window.getComputedStyle(text).fontSize);
+
+  increaseFont.addEventListener("click", function () {
+    if (currentFontSize >= 100) return;
+    ++currentFontSize;
+    text.style.fontSize = `${currentFontSize}px`;
+  });
+  decreaseFont.addEventListener("click", function () {
+    if (currentFontSize <= 1) return;
+    --currentFontSize;
+    console.log(currentFontSize);
+    text.style.fontSize = `${currentFontSize}px`;
   });
 });
