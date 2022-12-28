c883110be714de0fc52dd029ac5199ccee6cf32d
Angelo Longoria <42070067+arlongoria93@users.noreply.github.com>
2022-12-28 09:39:21 -0800

    crane game solution draft

--- DOM Events/index.js
+++ DOM Events/index.js
@@ -33,4 +33,35 @@ document.addEventListener("DOMContentLoaded", function () {
     colorPicker.value = colorPicker.value.toLowerCase();
     body.style.backgroundColor = colorPicker.value;
   });
+
+  /* Crane Game */
+  const craneImage = document.querySelector("#craneImage");
+  const leftButton = document.querySelector("#leftButton");
+  const rightButton = document.querySelector("#rightButton");
+  const upButton = document.querySelector("#upButton");
+  const downButton = document.querySelector("#downButton");
+
+  leftButton.addEventListener("click", function () {
+    //make image relative to parent
+    craneImage.style.position = "relative";
+    const currentLeft = parseInt(window.getComputedStyle(craneImage).left);
+    //move image left
+    craneImage.style.left = `${currentLeft - 10}px`;
+  });
+  rightButton.addEventListener("click", function () {
+    craneImage.style.position = "relative";
+    const currentRight = parseInt(window.getComputedStyle(craneImage).left);
+    console.log(currentRight);
+    craneImage.style.left = `${currentRight + 10}px`;
+  });
+  upButton.addEventListener("click", function () {
+    craneImage.style.position = "relative";
+    const currentUp = parseInt(window.getComputedStyle(craneImage).top);
+    craneImage.style.top = `${currentUp - 10}px`;
+  });
+  downButton.addEventListener("click", function () {
+    craneImage.style.position = "relative";
+    const currentDown = parseInt(window.getComputedStyle(craneImage).top);
+    craneImage.style.top = `${currentDown + 10}px`;
+  });
 });
