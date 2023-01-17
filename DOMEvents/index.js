a37b326d9c16e52dbab9719b8bcc4f2300f772a9
sllozier <sarah.lozier@gmail.com>
2023-01-17 10:39:44 -0500

    added to readme, added comments

--- DOMEvents/index.js
+++ DOMEvents/index.js
@@ -1,12 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // cookie clicker
+  //EXERCISE 1: Cookie Clicker
+  //Assign variable to the element of "cookie" id
+  // and variable to the element of "counter" id.
+  //Add the event listener as "click" to the "cookie" variable
+  // we just created. When we "click" the "cookie" the "counter"
+  // text will increment up one.
   const cookie = document.querySelector("#cookie");
   const counter = document.querySelector("#counter");
   cookie.addEventListener("click", function () {
     counter.textContent++;
   });
 
-  // magnify text
+  //EXERCISE 2: Magnify Text
+  //Assign variables to the elements of "text", "plusFont", and "minusFont" ids.
+  //Checkout how .getComputedStyle works here: https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
+  //Add the event listener as "click" to the "increaseFont" and "decreaseFont"
+  // variables. Increase font will increment the font size as long as it is not
+  // already 100px. Dont forget to set the text font size to the acceptable string
+  // data. Repeat these steps but instead decrement the font size as long as it is
+  // larger or equal to 1px.
   const text = document.querySelector("#text");
   const increaseFont = document.querySelector("#plusFont");
   const decreaseFont = document.querySelector("#minusFont");
@@ -24,7 +36,13 @@ document.addEventListener("DOMContentLoaded", function () {
     text.style.fontSize = `${currentFontSize}px`;
   });
 
-  //Change background color
+  //EXERCISE 3: Green Screen
+  //Assign variables to the elements of "body" and "greenScreenColor" and
+  // "greenScreenButton" ids.
+  //Add event listener as "click" to the "colorPickerButton" variable.
+  // Set the value of the "colorPicker" variable to lower case - this will
+  // prevent any unnecessary errors in the function. Then set the body
+  // background color to the value of the "colorPicker" variable.
   const body = document.querySelector("body");
   const colorPicker = document.querySelector("#greenScreenColor");
   const colorPickerButton = document.querySelector("#greenScreenButton");
@@ -34,7 +52,14 @@ document.addEventListener("DOMContentLoaded", function () {
     body.style.backgroundColor = colorPicker.value;
   });
 
-  /* Crane Game */
+  //EXTENSION: Crane Game
+  //Assign variables to the elements of "craneImage", "leftButton", "rightButton",
+  // "upButton" and "downButton" ids.
+  //Add event listenter as "click" to "leftButton", "rightButton", "upButton", and
+  // "downButton" variables. Set each one to a relative position to start.
+  //Create a new "current" variable for each that will hold the current position of
+  // the crane. This is so the crane will move based on current position after clicks
+  // have happened. After that, change the position by 10px in the direction selected.
   const craneImage = document.querySelector("#craneImage");
   const leftButton = document.querySelector("#leftButton");
   const rightButton = document.querySelector("#rightButton");
@@ -44,32 +69,57 @@ document.addEventListener("DOMContentLoaded", function () {
   leftButton.addEventListener("click", function () {
     //make image relative to parent
     craneImage.style.position = "relative";
+    //get current position
     const currentLeft = parseInt(window.getComputedStyle(craneImage).left);
     //move image left
     craneImage.style.left = `${currentLeft - 10}px`;
   });
   rightButton.addEventListener("click", function () {
+    //make image relative to parent
     craneImage.style.position = "relative";
+    //get current position
     const currentRight = parseInt(window.getComputedStyle(craneImage).left);
-    console.log(currentRight);
+    //move image right
     craneImage.style.left = `${currentRight + 10}px`;
   });
   upButton.addEventListener("click", function () {
+    //make image relative to parent
     craneImage.style.position = "relative";
+    //get current position
     const currentUp = parseInt(window.getComputedStyle(craneImage).top);
+    //move image up
     craneImage.style.top = `${currentUp - 10}px`;
   });
   downButton.addEventListener("click", function () {
+    //make image relative to parent
     craneImage.style.position = "relative";
+    //get current position
     const currentDown = parseInt(window.getComputedStyle(craneImage).top);
+    //move image down
     craneImage.style.top = `${currentDown + 10}px`;
   });
-  // Dice
+
+  //EXTENSION: Dice
+  //Assign variables to "diceNubmer", "rollDice", and "diceOutput" ids.
+  //Add event listener as "click" to "diceButton". Create "randomNumber"
+  // variable inside the event listener function that generates a random
+  // number value based on the input in variable "diceNumber";
+
+  //Generic random number function:
+  //  function generateRandomInteger(max){
+  //  return Math.floor(Math.random() * max) + 1;
+  //}
+  //  generateRandomInteger(10); this will return a random number between
+  // 0 and 10. Learn more about generating random numbers here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
   const diceNumber = document.querySelector("#diceNumber");
   const diceButton = document.querySelector("#rollDice");
   const diceOutput = document.querySelector("#diceOutput");
+
   diceButton.addEventListener("click", function () {
+    //"diceNumber" is our max
     const randomNumber = Math.floor(Math.random() * diceNumber.value) + 1;
+    // set "diceOuput" text to the random number we just created.
     diceOutput.textContent = randomNumber;
   });
 });
--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# Foundations.DOM.Events.Pair.Programming.Solution
+# Foundations.DOM.Pair.Programming.Events.Solution
+
+## Overview
+
+In this activity, you will be writing HTML, CSS, and JS to fulfill the requirements of progressively more difficult prompts. Try to get as far as you can through the prompts.
+
+## Getting Started
+
+1. The Driver should create a repository and add the Navigator as a collaborator.
+2. Clone the repository. Both partners should have local copies.
+3. Swap the Driver/Navigator roles after each exercise.
+4. Use Git and Github to collaborate. Remember to pull and push.
+
+## Instructions
+
+For each exercise:
+
+1. Create a new folder within your repository.
+2. In that folder, create `index.html`, `index.js` , and `style.css`.
+3. Write HTML to have all the required elements.
+4. Write CSS to style the elements.
+5. Write JS so that the elements are interactive.
+6. Double-check your code.
+
+These prompts leave many details up to your discretion. You are encouraged to add flavor text, different fonts, and cool styles. Add your own flair to your websites.
+
+## Tips
+
+If you are feeling stuck, try asking yourself the following questions:
+
+1. Are all the required HTML elements on the page?
+2. Are the DOM selectors selecting the correct elements?
+3. Are event listeners added to the specified elements?
+4. Are the event listeners listening for the correct event?
+5. Does the callback function modify the correct elements?
+
+There are five tabs (one for each exercise) to help you stay organized as you work. Your goal is to complete exercises 1, 2, and 3. If you finish early, see if you can complete one or both of the extension exercises.
+
+### Exercise 1 | Cookie Clicker
+
+Your website should have two main components: an image of a cookie and a counter that starts at 0. Whenever the cookie is clicked, the counter should go up by one.
+
+### Exercise 2 | Magnifying Glass
+
+Your website should have a paragraph of placeholder text, along with two buttons. One button should make the text bigger when clicked. The other button should make the text smaller when clicked. The font size should not get smaller than 1px and should not get larger than 100px.
+
+### Exercise 3 | Green Screen
+
+Your website should have a text input box with a corresponding button. When the button is clicked, the background color of the entire website should change to match what the user typed into the box. If the user did not input a valid color name, then nothing should happen.
+
+Note: Your website should support at least five valid colors.
+
+### Extension | Crane Game
+
+Your website should show an image of a crane alongside four buttons. Each button corresponds to a cardinal direction (up, down, left, right), and will move the crane in that direction when clicked.
+
+### Extension | Dice
+
+Your website should have a text input box labeled "Number of sides" and a button labeled "Roll". When the button is clicked, a random number will appear between 1 and the number that the user input. For example, if the user types 6 into the box and clicks the button, then a number between 1 to 6 will appear. A different random number should be generated every time that button is clicked.
+
+**Done? Make sure you have completed the first three exercises in the tabs above.**
